3a999adbdbc8c4e4f688da35b9404eff0899d129
jonasmstuart13 <54625051+jonasmstuart13@users.noreply.github.com>
2019-09-12 15:26:04 +0700

    Update RandoMorley.js

--- RandoMorley.js
+++ RandoMorley.js
@@ -1,1 +1,17 @@
-
+randomFunctions = {
+  this.numbers = [],
+  this.min = 0,
+  this.max = 0,
+  generateNumber(min, max){
+    var range = max-min
+    var number = Math.round(Math.random()*range)+min
+    return number
+  },
+  loopThroughGenerateNumber(n){
+    for (var i = 0; i < n; i++){
+      var numToAdd = this.generateNumber(min,max)
+      this.numbers.push(numToAdd)
+    }
+  return this.numbers
+  } 
+},
