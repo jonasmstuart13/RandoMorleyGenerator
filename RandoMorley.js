00f8d87d1af1676a53a3c5571647d2d6d643bd40
jomostu13 <jonasmstuart13@gmail.com>
2019-09-12 20:48:11 +0700

    sept 12 evening work

--- RandoMorley.js
+++ RandoMorley.js
@@ -1,17 +1,50 @@
-randomFunctions = {
-  this.numbers = [],
-  this.min = 0,
-  this.max = 0,
-  generateNumber(min, max){
-    var range = max-min
-    var number = Math.round(Math.random()*range)+min
-    return number
+var genNumber = document.getElementById("genNumBtn");
+var genNumberResult = document.getElementById("genNumberResult");
+var genNumberList = document.getElementById("genNumberListBtn");
+var numberListResult = document.getElementById("numberListResult");
+var refreshList = document.getElementById("refreshListBtn");
+
+refreshList.addEventListener("click", clear);
+function clear() {
+  randomFunctions.refreshList();
+}
+
+genNumberList.addEventListener("click", runList);
+function runList() {
+  var minimum = parseFloat(document.getElementById("min").value);
+  var maximum = parseFloat(document.getElementById("max").value);
+  var iterationInput = parseFloat(
+    document.getElementById("iterationInput").value
+  );
+  console.log(iterationInput);
+  randomFunctions.loopThroughGenerateNumber(iterationInput, minimum, maximum);
+}
+
+genNumber.addEventListener("click", run);
+function run() {
+  var minimum = parseFloat(document.getElementById("min").value);
+  var maximum = parseFloat(document.getElementById("max").value);
+  randomFunctions.generateNumber(minimum, maximum);
+}
+
+var randomFunctions = {
+  numbers: [],
+  generateNumber(n, x) {
+    var range = x - n;
+    var number = Math.round(Math.random() * range) + n;
+    genNumberResult.innerHTML = number;
+    return number;
   },
-  loopThroughGenerateNumber(n){
-    for (var i = 0; i < n; i++){
-      var numToAdd = this.generateNumber(min,max)
-      this.numbers.push(numToAdd)
+  loopThroughGenerateNumber(i, n, x) {
+    for (var j = 0; j < i; j++) {
+      var numToAdd = this.generateNumber(n, x);
+      this.numbers.push(numToAdd);
     }
-  return this.numbers
-  } 
-},
+    numberListResult.innerHTML = this.numbers;
+    return this.numbers;
+  },
+  refreshList() {
+    numberListResult.innerHTML = "";
+    this.numbers = [];
+  }
+};
---
+++ RandoMorleyJStoHTML.js
@@ -0,0 +1,11 @@
+import randomFunctions from "/RandoMorley";
+var minimum = document.getElementById("min").value;
+var maximum = document.getElementById("max").value;
+var genNumber = document.getElementById("genNumBtn");
+var genNumberResult = document.getElementById("genNumberResult");
+alert("hi");
+
+genNumber.addEventListener("click", generateNum());
+function generateNum() {
+  alert("hi");
+}
